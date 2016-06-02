ca85be4671250c1a976b2c4cefdb50b03fe6f5e1
Jessica <jessicaoliveira3108@gmail.com>
2016-06-02 09:16:38 -0300

    first commit

--- Tesla Concursos Noticias/LoginViewController.swift
+++ Tesla Concursos Noticias/LoginViewController.swift
@@ -14,10 +14,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+            
+
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent;
     }
 
 
